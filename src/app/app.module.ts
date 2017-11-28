9bc3be7e214a1e986a729e848b08c7e09ab24f7e
ralph <ralph.garcia@ivanti.com>
2017-11-27 16:56:35 -0800

    add scss from old prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {Component} from '@angular/core';
 import { AppComponent } from './app.component';
 
+// Include MD animations
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Include MD components
+import { MatIconModule, MatSidenavModule, MatToolbarModule, MatTabsModule, MatSelectModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatMenuModule, MatListModule, MatGridListModule, MatCardModule } from '@angular/material';
+// Include gesture support
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // MD Modules
+    MatIconModule, 
+    MatSidenavModule, 
+    MatToolbarModule, 
+    MatTabsModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatMenuModule, 
+    MatListModule, 
+    MatGridListModule, 
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
