c7cb631a63e2649d6eeb870bfc64af0cd36922b9
ralph <ralph.garcia@ivanti.com>
2017-11-29 12:09:37 -0800

    overwrite md button styles, add icon sizing, css org updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core'; //To enable encapsulation changes
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.Emulated is default 
+  // encapsulation: ViewEncapsulation.Native
 })
 export class AppComponent {
   title = 'Ivanti Prototyping';
